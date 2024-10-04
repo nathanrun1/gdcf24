5c8c45475036b09893943045e29b7091c30889c2
nathanrun1 <nathanmreilly@gmail.com>
2024-10-04 19:02:02 -0400

    Player movement, Tilemap, basic collision, placeholder sprites

---
+++ scripts/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+class_name Player
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _physics_process(delta: float) -> void:
+
+	var directionx := Input.get_axis("ui_left", "ui_right")
+	if directionx:	
+		velocity.x = directionx * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var directiony := Input.get_axis("ui_up", "ui_down")
+	if directiony:
+		velocity.y = directiony * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+	
+	move_and_slide()
