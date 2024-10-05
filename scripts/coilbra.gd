53617ac13ab15e02949852e9599de4017a939d92
Alexander Feng <alexanderfzz@Alexanders-MacBook-Pro.local>
2024-10-05 12:34:57 -0400

    animation update

--- scripts/coilbra.gd
+++ scripts/coilbra.gd
@@ -3,20 +3,16 @@ extends CharacterBody2D
 @onready var sprite_anm = $AnimatedSprite2D
 @onready var player := $"../Player"
 
-const SPEED : int = 35;
+const SPEED : int = 80
+const STAGGER_TIME : int = 4
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta: float) -> void:	
 	_play_animation()
 	
 	
 	var direction : Vector2 = global_position.direction_to(player.global_position)
 	velocity = SPEED * direction
-	
-	if (direction.x > 0): # right facing
-		sprite_anm.transform("right")
-	else: #left facing
-		sprite_anm.transform("left")
 	
 	
 	move_and_collide(velocity * delta)
@@ -24,3 +20,4 @@ func _physics_process(delta: float) -> void:
 
 func _play_animation() -> void:
 	sprite_anm.play("default");
+	
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,24 +5,25 @@ class_name Player
 const SPEED = 100
 
 func _physics_process(delta: float) -> void:
+	_play_animation()
+	
+	
+	var direction : Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * SPEED
+	
+	if (direction.x > 0): # right facing
+		sprite_anm.transform("right")
+	else: #left facing
+		sprite_anm.transform("left")
+	
+	
+	move_and_collide(velocity * delta)
+	
+	
 
-	var directionx := Input.get_axis("ui_left", "ui_right")
-	if directionx:	
-		velocity.x = directionx * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	var directiony := Input.get_axis("ui_up", "ui_down")
-	if directiony:
-		velocity.y = directiony * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-		
-	
-	move_and_slide()
-	
+
+func _play_animation() -> void:
 	if (velocity.length() > 0.0):
 		sprite_anm.play("move")
 	else:
 		sprite_anm.play("idle")
-	
